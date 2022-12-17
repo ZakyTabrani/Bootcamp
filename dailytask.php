d5dc4524f4d0c46b81333fe78e9579941ea2e3e0
ZakyTabrani <120723287+ZakyTabrani@users.noreply.github.com>
2022-12-17 12:19:09 +0700

    Add files via upload

---
+++ dailytask.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "Tipe data adalah bahasa dalam program untuk menentukan value dari suatu operasi <br>" ;
+	echo '<br>' ;
+echo "Contoh tipe data : <br>" ;
+	echo "1. int = merupakan bilangan bulat <br>" ;
+	echo "2. floating = bilangan pecahan atau desimal <br>" ;
+	echo "3. char = menyimpan karakter alfabet <br>" ;
+	echo "4. boolean = mencari value true or false <br>" ;
+	echo "5. array = menyimpan sebuah element <br>" ;
+	echo "6. string = mencakup teks dan diberi kutip <br>" ;
+
+	echo '<br>' ;
+echo "Aritmatika dalam pemrograman adalah simbol yang menandakan suatu operasi, baik itu penjumlahan atau pengurangan dll <br>" ;
+	$a = 5;
+	$b = 5;
+
+$jawab1=$a + $b;
+$jawab2=$a - $b;
+$jawab3=$a / $b;
+$jawab4=$a % $b;
+
+	echo '<br>' ;
+echo "a = 5 dan b = 5 <br>" ;
+echo " penjumlahan : $jawab1 <br>" ;
+echo " pengurangan : $jawab2 <br>" ;
+echo " pembagian : $jawab3 <br>" ;
+echo " modulus : $jawab4 <br>" ;
+?>
