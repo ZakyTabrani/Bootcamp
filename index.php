af5419392e2eb94292d405131dfb01ef3ab30848
ZakyTabrani <120723287+ZakyTabrani@users.noreply.github.com>
2022-12-23 16:53:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 45 ;
+
+if ($a < 45) {
+	echo "Nilai: D <br>";
+	echo "Keterangan : Tidak lulus";
+}else if ($a == 45) {
+	echo "Nilai : C <br>";
+	echo "Keterangan : Cukup";
+}else if ($a <70) {
+	echo "Nilai : B <br>";
+	echo "Keterangan : Baik";
+}else if ($a >70) {
+	echo "Nilai : A <br>";
+	echo "Keterangan : Mantab";
+}
+
+?>
