656440e2d1505b19f902b2192a26ae071070e5d8
ZakyTabrani <120723287+ZakyTabrani@users.noreply.github.com>
2023-01-07 12:09:38 +0700

    Add files via upload

---
+++ loop-for.php
@@ -0,0 +1,33 @@
+<?php
+
+// for ($i = 0 ; $i <= 10 ; $i++) {
+// 	$data = $i * 2;
+
+// 	echo "$data ";
+// }
+
+// for ($i = 10 ; $i >= 1 ; $i--) {
+// 		$data = $i * 2;
+
+// 		echo "$data ";
+// }
+
+// for ($i =1; $i <= 5; $i++) {
+
+// 	for ($j = 1; $j <= $i; $j++) {
+// 		echo "* ";
+// 	}
+
+// 	echo "<br/>";
+// }
+
+for ($i =1; $i <= 10; $i++) {
+
+	for ($j = 1; $j <= $i; $j++) {
+		$data = $i * $j;
+
+		echo "$data ";
+	}
+
+	echo "<br/>";
+}
---
+++ loop-while.php
@@ -0,0 +1,11 @@
+<?php
+
+	$index = 1;
+
+	while ($index <= 10) {
+		$angka = pow ($index, 3);
+
+		echo "$angka ";
+
+		$index++;
+	}
